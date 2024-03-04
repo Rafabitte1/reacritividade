41b59590b16d6596d4cd6a51a6b47c12b43b1e6d
Rafabitte1 <rafabitte1@gmail.com>
2024-03-04 15:11:47 -0300

    feito

---
+++ src/App.vue
@@ -0,0 +1,41 @@
+<script setup>
+import { ref } from 'vue'
+
+const contador = ref(0)
+
+const contador2 = ref(0);
+
+function decrementar() {
+  contador.value--
+ 
+}
+
+function encrementar() {
+  contador.value++
+}
+
+function decrementar2(){
+    contador2.value--
+}
+
+function encrementar2(){
+    contador2.value++
+}
+
+</script>
+<template>
+    <div class="contador "><h1>Contador</h1>
+      <button @click="encrementar">+</button>
+      <button @click="decrementar">-</button>
+      <p>O valor do contador é: {{ contador }}</p>
+    </div>
+    <div class="contador 2" ><h1>Contador2</h1>
+      <button @click="encrementar2">+</button>
+      <button @click="decrementar2">-</button>
+      <p>O valor do contador é: {{ Contador2 }}</p></div>
+
+      <div>
+        <p v></p>
+
+      </div>
+</template>
